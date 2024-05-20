ad16587d7e033a7d74caefec61e4e337d6e97604
dark-tracer <bernieamponsah2@gmail.com>
2024-05-20 10:50:28 -0700

    0x15-api

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,24 +1,37 @@
- #!/usr/bin/python3
- """ Export api to csv"""
- import csv
- import requests
- import sys
+#!/usr/bin/python3
+"""
+get data from an api based on the userd id that is passed as argument
+"""
 
- if __name__ == '__main__':
-     user = sys.argv[1]
-     url_user = 'https://jsonplaceholder.typicode.com/users/' + user
-     res = requests.get(url_user)
-     """ANYTHING"""
-     user_name = res.json().get('username')
-     task = url_user + '/todos'
-     res = requests.get(task)
-     tasks = res.json()
+import csv
+import requests
+from sys import argv
 
-     with open('{}.csv'.format(user), 'w') as csvfile:
-         for task in tasks:
-             completed = task.get('completed')
-             """Complete"""
-             title_task = task.get('title')
-             """Done"""
-             csvfile.write('"{}","{}","{}","{}"\n'.format(
-                 user, user_name, completed, title_task))
+
+if __name__ == "__main__":
+    
+    if len(argv) == 2:
+        url_base = 'https://jsonplaceholder.typicode.com/users/'
+        person_url = '{}{}'.format(url_base, argv[1])
+        todos_url = '{}{}/{}'.format(url_base, argv[1], 'todos')
+
+        """ do two request
+        one for user personal info and for todos tasks
+        """
+        person_res = requests.get(person_url)
+        todos_res = requests.get(todos_url)
+
+        """ get the obj responses body"""
+        person_obj = person_res.json()
+        todos_obj = todos_res.json()
+
+        """ working with the data """
+        name = person_obj['username']
+        filename = '{}.csv'.format(person_obj['id'])
+
+        with open(filename, 'w', newline='') as f:
+            for obj in todos_obj:
+                line = [obj['userId'], name, obj['completed'], obj['title']]
+                writer = csv.writer(f, delimiter=',', quotechar='"',
+                        quoting=csv.QUOTE_ALL)
+                writer.writerow(line)
